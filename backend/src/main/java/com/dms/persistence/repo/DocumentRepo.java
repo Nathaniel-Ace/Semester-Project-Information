823114a7c48c4b85a0d0d7ef8086cf00ae2fabf4
Nathaniel-Ace <nathanielacep@gmail.com>
2024-11-25 13:23:29 +0100

    Revert "Merge pull request #16 from Nathaniel-Ace/frontend"
    This reverts commit fa9287f81007e7e1894a03be5ddc720b6a6395b4, reversing
    changes made to dafb0c49fe9ac3b4f4e56fa652aa2f6e06c61984.

--- backend/src/main/java/com/dms/persistence/repo/DocumentRepo.java
+++
@@ -1,8 +0,0 @@
-package com.dms.persistence.repo;
-
-import com.dms.persistence.entity.Document;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DocumentRepo extends JpaRepository<Document, Long> {
-    Document findByTitle(String name);
-}
