183a446b1ca6713ad3fa2e2ee970c88f6fb5f3da
Nathaniel-Ace <nathanielacep@gmail.com>
2024-12-03 11:16:21 +0100

    unused Imports entfernt

--- backend/src/main/java/com/dms/config/RabbitMQConfig.java
+++ backend/src/main/java/com/dms/config/RabbitMQConfig.java
@@ -2,7 +2,6 @@ package com.dms.config;
 
 import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
